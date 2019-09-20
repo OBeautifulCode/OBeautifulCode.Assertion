95b73a70cd4fddb7db7c62d236693f4aec3a9d88
Suraj Gupta <SurajGupta@users.noreply.github.com>
2019-09-20 13:07:55 -0400

    comment fix

--- OBeautifulCode.Validation.ParameterValidator/ParameterValidation.Utility.cs
+++ OBeautifulCode.Validation.ParameterValidator/ParameterValidation.Utility.cs
@@ -194,8 +194,8 @@ namespace OBeautifulCode.Validation.Recipes
         {
             // A copy of this method exists in OBC.Reflection.
             // Any bug fixes made here should also be applied to OBC.Reflection.
-            // OBC.Reflection cannot take a reference to OBC.Validation because it creates a circular reference
-            // since OBC.Validation itself depends on OBC.Reflection.
+            // OBC.Validation cannot take a reference to OBC.Reflection because it creates a circular reference
+            // since OBC.Reflection itself depends on OBC.Validation.
             // We considered converting all usages of OBC.Validation in OBC.Reflection to vanilla if..then..throw
             // but decided against because it was going to be too much work and we like the way OBC.Validation reads (e.g. Must().NotBeNull()) in OBC.Reflection.
             // The other option was to create a third package that OBC.Validation and OBC.Reflection could both depend on, but
